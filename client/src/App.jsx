60d37fdb2c16b3707e024501341727773c221d59
jbluemlein <jbluemlein@gmail.com>
2025-07-09 18:16:01 -0500

    test

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="p-4 text-center text-lg">
+      <h1 className="text-2xl font-bold">📚 Book Recommender MVP</h1>
+      <p className="mt-4">Frontend is working with TailwindCSS!</p>
+    </div>
+  );
+}
+
+export default App;
